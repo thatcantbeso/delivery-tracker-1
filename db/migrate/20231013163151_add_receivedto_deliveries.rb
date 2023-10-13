d80dc112c8db3e622e4e869de678192f5fbbd9a0
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-13 17:28:50 +0000

    quicksave

---
+++ db/migrate/20231013163151_add_receivedto_deliveries.rb
@@ -0,0 +1,4 @@
+class AddReceivedtoDeliveries < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_190653) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_163151) do
   create_table "deliveries", force: :cascade do |t|
     t.string "description"
     t.date "supposed_to_arrive_on"
